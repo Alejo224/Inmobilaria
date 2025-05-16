e78dba227b404dc6676f0b519f2b10c95d7b39e2
Kevin <zuluaga.kevin@correounivalle.edu.co>
2025-05-15 20:37:19 -0500

    prueba de carpeta vista

---
+++ src/main/java/vista/AgenteComercial.java
@@ -0,0 +1,10 @@
+package vista;
+
+import javax.swing.*;
+
+public class AgenteComercial {
+    private JButton registrarClienteButton;
+    private JPanel panel1;
+    private JButton contratoPropietarioInmobliariaButton;
+    private JButton contratoClienteInmbiliariaButton;
+}
