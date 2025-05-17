26c86f6db897ea373bb90e2ce79f4485806a29e1
Alejo224 <johantorresalejo19@gmail.com>
2025-05-17 14:33:49 -0500

    feat: configuración la conexión de DB con env vars. y agregando las dependencias en pom.xml (mysql y postgresql).• Lee DB_URL, DB_USER, DB_PASSWORD desde variables de entorn

--- src/main/java/com/inmobilaria/modelo/ConexionDB.java
+++ src/main/java/com/inmobilaria/modelo/ConexionDB.java
@@ -1,4 +1,41 @@
 package com.inmobilaria.modelo;
 
+import java.sql.*;
+
+/**
+ * Configuración de la conexión JDBC de forma centralizada.
+**/
+
 public class ConexionDB {
+
+    Connection connection = null;
+    private final String user = System.getenv("DB_USER");
+    private final String password = System.getenv("DB_PASSWORD");
+    private final String url = System.getenv("DB_URL");
+
+    private static final String DRIVER_MSQL = "com.mysql.cj.jdbc.Driver";
+    private static final String DRIVER_POSTGRESQL = "org.postgresql.Driver";
+
+    public Connection establecerConexion() {
+        System.out.println("Conectando ...");
+        try {
+            Class.forName(DRIVER_MSQL);
+            connection = java.sql.DriverManager.getConnection(url, user, password);
+            System.out.println("Se conecto a la base de datos correctamente");
+        } catch (Exception e) {
+            System.out.println("Error al conectar a la base de datos: " + e);
+        }
+        return connection;
+    }
+
+    public void cerrarConexion(){
+        try{
+            if (connection != null && !connection.isClosed() ){
+                connection.close();
+                System.out.println("La conexion fue cerrada correctamente");
+            }
+        } catch (SQLException e){
+            System.out.println("La conexion no fue cerrada correctamente \n " + e);
+        }
+    }
 }
